5c5b8b4a93779e56dcff631a87b2b0f49c77e717
Brian Marquis <rbmarquis.dev@gmail.com>
2020-06-28 21:29:52 -0500

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>Oops? That page doesn&#39;t exist.</p>
+    </div>
+  )
+}
